13d5a09e3f8a7c61df39da86003fe637380b7b22
jason <jk17@ualberta.ca>
2014-11-25 22:31:39 -0800

    added wal-e, made the box configurable so that we can support multiple modes (master/slave) and recover a backup before starting postgres

--- Dockerfile
+++ Dockerfile
@@ -8,17 +8,25 @@ COPY ./postgresql.conf /etc/postgresql/$VERSION/main/
 # Set default environment modes
 
 # determines if the postgres server is configured as a master or a slave.
+# master nodes generate the backups and the logs (wal-e is running in archive mode)
+# slave nodes can only consume logs, DEFAULT, this is so that if we forget to set the value we dont accidently clobber our backups
 # this can be one of the following: ["master", "slave"]
-ENV DOCKER_POSTGRES_MODE master
+ENV DOCKER_POSTGRES_MODE slave
 
 # determines if a wal-e database backup should be recovered before starting postgres.
 # this can be one of the following: ["true", "false"]
 ENV DOCKER_POSTGRES_RECOVER false #this can be one of the following: ["true", "false"]
 
 # if DOCKER_POSTGRES_RECOVER is true, this value determines which backup is restored
-# the latest, or a specific date. ['latest', eg. '2012-03-06 16:38:00']
-ENV DOCKER_POSTGRES_RECOVER_FROM latest
+# the latest, or a specific date. ['LATEST', eg. '2012-03-06 16:38:00']
+ENV DOCKER_POSTGRES_RECOVER_FROM LATEST
 
+# create conf.d folder to load postgres config files.
+RUN mkdir -m 755 -p /etc/postgresql/$VERSION/main/conf.d
+
+# copy the wal-e crontab file
+# Crontab that does a full backup daily at 2AM and deletes old backups (retaining 7 previous backups) at 3AM:
+COPY ./cron/wal-e     /etc/cron.d/wal-e
 
 # copy over startup script
 COPY startup.sh /data/scripts/startup.sh
--- startup.sh
+++ startup.sh
@@ -1,5 +1,25 @@
 #recovery mode
 
+if [ $DOCKER_POSTGRES_MODE -eq "master" ]; then
+    echo "wal_level = hot_standby # hot_standby is also acceptable (will log more)"         >> /etc/postgresql/9.3/main/conf.d/10master.conf
+    echo "archive_mode = on"                                                            >> /etc/postgresql/9.3/main/conf.d/10master.conf
+    echo "archive_command = 'envdir /etc/wal-e.d/env wal-e wal-push %p'"                >> /etc/postgresql/9.3/main/conf.d/10master.conf
+    echo "archive_timeout = 60"                                                         >> /etc/postgresql/9.3/main/conf.d/10master.conf
+else
+    #slave mode (DEFAULT) readonly, will not actually create wal-e archives
+    echo "wal_level = hot_standby # hot_standby is also acceptable (will log more)"         >> /etc/postgresql/9.3/main/conf.d/05slave.conf
+
+fi
+
+if [$DOCKER_POSTGRES_RECOVER -eq "true"]; then
+    envdir /etc/wal-e.d/pull-env wal-e backup-fetch /var/lib/postgresql/9.3/main $DOCKER_POSTGRES_RECOVER_FROM
+
+    echo "standby_mode     = 'on'"                                                      >> /var/lib/postgresql/9.3/main/recovery.conf
+    #echo "primary_conninfo = 'host=$HOST user=$USER password=$PASSWORD'"                >> /var/lib/postgresql/9.3/main/recovery.conf
+    echo "restore_command  = 'envdir /etc/wal-e.d/env wal-e wal-fetch \"%f\" \"%p\"'"   >> /var/lib/postgresql/9.3/main/recovery.conf
+    echo "trigger_file     = '/var/lib/postgresql/9.3/main/trigger'"                       >> /var/lib/postgresql/9.3/main/recovery.conf
+fi
+
 
 
 #start phusion baseimage runner (https://github.com/phusion/baseimage-docker)
