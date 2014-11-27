da0423c50ab38f41e9a043026f5d69411caeea14
jason <jk17@ualberta.ca>
2014-11-26 22:52:26 -0800

    delete and recreate the data directory on restore.

--- startup.sh
+++ startup.sh
@@ -1,29 +1,37 @@
 #!/usr/bin/env bash
 VERSION=9.3
 
+CONFIG_DIR=/etc/postgresql/$VERSION/main
+DATA_DIR=/var/lib/postgresql/$VERSION/main
+
 # create conf.d folder to load postgres config files.
-mkdir -m 755 -p /etc/postgresql/$VERSION/main/conf.d/
+mkdir -m 755 -p $CONFIG_DIR/conf.d/
 
 if [ "$DOCKER_POSTGRES_MODE" = "leader" ];
 then
-    echo "wal_level = hot_standby # hot_standby is also acceptable (will log more)"     >> /etc/postgresql/$VERSION/main/conf.d/10leader.conf
-    echo "archive_mode = on"                                                            >> /etc/postgresql/$VERSION/main/conf.d/10leader.conf
-    echo "archive_command = 'envdir /etc/wal-e.d/env wal-e wal-push %p'"                >> /etc/postgresql/$VERSION/main/conf.d/10leader.conf
-    echo "archive_timeout = 60"                                                         >> /etc/postgresql/$VERSION/main/conf.d/10leader.conf
+    echo "wal_level = hot_standby # hot_standby is also acceptable (will log more)"     >> $CONFIG_DIR/conf.d/10leader.conf
+    echo "archive_mode = on"                                                            >> $CONFIG_DIR/conf.d/10leader.conf
+    echo "archive_command = 'envdir /etc/wal-e.d/env wal-e wal-push %p'"                >> $CONFIG_DIR/conf.d/10leader.conf
+    echo "archive_timeout = 60"                                                         >> $CONFIG_DIR/conf.d/10leader.conf
 else
     #follower mode (DEFAULT) readonly, will not actually create wal-e archives
-    echo "wal_level = hot_standby # hot_standby is also acceptable (will log more)"     >> /etc/postgresql/$VERSION/main/conf.d/10follower.conf
-    echo "hot_standby = on"                                                             >> /etc/postgresql/$VERSION/main/conf.d/10follower.conf
+    echo "wal_level = hot_standby # hot_standby is also acceptable (will log more)"     >> $CONFIG_DIR/conf.d/10follower.conf
+    echo "hot_standby = on"                                                             >> $CONFIG_DIR/conf.d/10follower.conf
 fi
 
 if [ "$DOCKER_POSTGRES_RECOVER" = "true" ];
 then
-    envdir /etc/wal-e.d/pull-env wal-e backup-fetch /var/lib/postgresql/$VERSION/main $DOCKER_POSTGRES_RECOVER_FROM
+    #delete the data folder completely.
+    rm -rf $DATA_DIR
+    #recreate it.
+    mkdir -m 755 -p $DATA_DIR
 
-    echo "standby_mode     = 'on'"                                                      >> /var/lib/postgresql/$VERSION/main/recovery.conf
-    #echo "primary_conninfo = 'host=$HOST user=$USER password=$PASSWORD'"               >> /var/lib/postgresql/$VERSION/main/recovery.conf
-    echo "restore_command  = 'envdir /etc/wal-e.d/env wal-e wal-fetch \"%f\" \"%p\"'"   >> /var/lib/postgresql/$VERSION/main/recovery.conf
-    echo "trigger_file     = '/var/lib/postgresql/$VERSION/main/trigger'"                    >> /var/lib/postgresql/$VERSION/main/recovery.conf
+    envdir /etc/wal-e.d/pull-env wal-e backup-fetch $DATA_DIR $DOCKER_POSTGRES_RECOVER_FROM
+
+    echo "standby_mode     = 'on'"                                                      >> $DATA_DIR/recovery.conf
+    #echo "primary_conninfo = 'host=$HOST user=$USER password=$PASSWORD'"               >> $DATA_DIR/recovery.conf
+    echo "restore_command  = 'envdir /etc/wal-e.d/env wal-e wal-fetch \"%f\" \"%p\"'"   >> $DATA_DIR/recovery.conf
+    echo "trigger_file     = '/var/lib/postgresql/$VERSION/main/trigger'"               >> $DATA_DIR/recovery.conf
 fi
 
 #start phusion baseimage runner (https://github.com/phusion/baseimage-docker)
