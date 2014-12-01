5b41e397bb4bbcb5296de43d5fc63d17ed10b6de
jason <jk17@ualberta.ca>
2014-12-01 01:50:45 -0800

    change ownership

--- scripts/first_run.sh
+++ scripts/first_run.sh
@@ -64,7 +64,7 @@ EOF
   chmod -R 755               /etc/cron.{d,daily,hourly,monthly,weekly}
   chown -R root:postgres     /etc/wal-e.d
   chmod -R 750               /etc/wal-e.d
-  chown -R postgres:postgres /etc/postgresql/9.3/main
+  chown -R postgres:postgres $CONFIG_DIR
   #chmod -R 700               /etc/postgresql/9.3/main
 }
 
