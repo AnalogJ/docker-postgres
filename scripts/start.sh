d3ddcd12f9ccbd7cae1d6105da41149ea1b879f6
jason <jk17@ualberta.ca>
2014-12-04 17:26:19 -0800

    wait for postgres to start, for a max of 5 minutes.

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,8 +14,17 @@ fi
 
 wait_for_postgres_and_run_post_start_action() {
   # Wait for postgres to finish starting up first.
-  while [[ ! -e /run/postgresql/9.3-main.pid ]] ; do
-      inotifywait -q -e create /run/postgresql/ >> /dev/null
+#  while [[ ! -e /run/postgresql/9.3-main.pid ]] ; do
+#      inotifywait -q -e create /run/postgresql/ >> /dev/null
+#  done
+
+  #wait a max of 5 minutes for postgres to startup.
+  n=0
+  until [ $n -ge 30 ]
+  do
+    su postgres -c "psql -t  -c \"select pg_postmaster_start_time()\" postgres" && break
+    n=$[$n+1]
+    sleep 10
   done
 
   post_start_action
